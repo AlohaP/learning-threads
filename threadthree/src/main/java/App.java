b38b9f68522929db1c44a4cdbddc832a2ffb88cc
Piotr Górski <piotrek.gorski90@gmail.com>
2018-08-08 20:45:18 +0200

    Use of synchronized keyword

---
+++ threadthree/src/main/java/App.java
@@ -0,0 +1,43 @@
+public class App {
+
+    private int count = 0;
+
+    private synchronized void increment(){
+        count++;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        App app = new App();
+        app.doWork();
+    }
+
+    public void doWork() throws InterruptedException {
+
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                for(int i = 0; i <10000; i++){
+                    increment();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                for(int i = 0; i <10000; i++){
+                   increment();
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Count is: " + count);
+
+    }
+}
